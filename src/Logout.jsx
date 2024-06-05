4b9c970d45f0bc0fe87213d82dbc25ba24af792f
oldmcdonnell <oldmcdonnell@gmail.com>
2024-06-05 07:48:34 -0400

    fucking stop

---
+++ src/Logout.jsx
@@ -0,0 +1,37 @@
+import { useContext, useEffect, useState } from "react"
+import { AuthContext } from "./context"
+import { useNavigate } from "react-router-dom"
+
+function Logout() {
+    const { auth } = useContext(AuthContext)
+    const token = localStorage.getItem("token")
+    const navigate = useNavigate()
+
+    useEffect(()=> {
+        if (auth.accessToken = undefined){
+          navigate('/login/')
+        }
+      },[navigate, auth.accessToken])
+
+
+    const logoutUser = (token) => {
+        auth.setAccessToken("")
+        localStorage.removeItem("token")
+        console.log('token should be clear', token)
+        
+    }
+
+    return(
+        <>
+        <div>
+            <button
+            onClick={logoutUser}
+            >
+            Logout
+            </button>
+            </div>
+        </>
+    )
+}
+
+export default Logout
